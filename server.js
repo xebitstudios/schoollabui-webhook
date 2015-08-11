4d129782dcd823dab5be2c55217f1647c81ba73e
Adetunji <xebitstudios@yahoo.com>
2015-08-10 21:59:43 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 8082;
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+	res.send('Hello World!');
+});
+
+app.post('/webhook', function(req, res) {
+	// We'll add this next
+});
+
+app.listen(port);
+
+console.log('Express server listening on port ' + port);
+
+// xebitstudios/schoollabui-webhook
+// https://github.com/xebitstudios/schoollabui-webhook.git - https
+// git@github.com:xebitstudios/schoollabui-webhook.git - ssh
